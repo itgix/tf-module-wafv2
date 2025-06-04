8f8214ad0cb532e5c60697f898a8c4814ce59852
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-06-04 13:42:36 +0300

    Reconfiguring custom managed rule

--- locals.tf
+++ locals.tf
@@ -19,9 +19,20 @@ locals {
     }
   ]
 
-  effective_custom_managed_waf_rule_groups = length(var.custom_managed_waf_rule_groups) > 0 ? var.custom_managed_waf_rule_groups : (
-    var.web_acl_scope == "CLOUDFRONT" ?
-      local.default_custom_managed_rule_groups_cloudfront :
-      local.default_custom_managed_rule_groups_regional
-  )
+  # Filter custom_managed_waf_rule_groups to only those matching scope
+  filtered_custom_managed_rule_groups = [
+    for r in var.custom_managed_waf_rule_groups : r
+    if (
+      (var.web_acl_scope == "CLOUDFRONT" && contains(r.rule_group_arn, ":global/")) ||
+      (var.web_acl_scope == "REGIONAL" && contains(r.rule_group_arn, ":regional/"))
+    )
+  ]
+
+  effective_custom_managed_waf_rule_groups = length(local.filtered_custom_managed_rule_groups) > 0
+    ? local.filtered_custom_managed_rule_groups
+    : (
+      var.web_acl_scope == "CLOUDFRONT" ?
+        local.default_custom_managed_rule_groups_cloudfront :
+        local.default_custom_managed_rule_groups_regional
+    )
 }
--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "webacl_arn" {
 output "webacl_id" {
   value = length(aws_wafv2_web_acl.wafv2_web_acl) > 0 ? aws_wafv2_web_acl.wafv2_web_acl[0].id : null
 }
+
+output "effective_custom_managed_waf_rule_groups" {
+  value = local.effective_custom_managed_waf_rule_groups
+}
