610a575a90908a03b5a52a9e13a166ce8bbd7908
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-06-04 22:10:04 +0300

    Reconfiguring custom managed rule

--- locals.tf
+++ locals.tf
@@ -1,26 +1,27 @@
 locals {
-  has_global_rule_group   = length(aws_wafv2_rule_group.custom_rule_group_global) > 0
-  has_regional_rule_group = length(aws_wafv2_rule_group.custom_rule_group_regional) > 0
+  default_custom_managed_rule_groups_cloudfront = (
+    length(aws_wafv2_rule_group.custom_rule_group_global) > 0 ? [
+      {
+        name                    = "CustomManagedRuleSetGlobal"
+        priority                = 1
+        action                  = "none"
+        rule_group_arn          = aws_wafv2_rule_group.custom_rule_group_global[0].arn
+        rules_override_to_count = []
+      }
+    ] : []
+  )
 
-  default_custom_managed_rule_groups_cloudfront = local.has_global_rule_group ? [
-    {
-      name                    = "CustomManagedRuleSetGlobal"
-      priority                = 1
-      action                  = "none"
-      rule_group_arn          = aws_wafv2_rule_group.custom_rule_group_global[0].arn
-      rules_override_to_count = []
-    }
-  ] : []
-
-  default_custom_managed_rule_groups_regional = local.has_regional_rule_group ? [
-    {
-      name                    = "CustomManagedRuleSetRegional"
-      priority                = 1
-      action                  = "none"
-      rule_group_arn          = aws_wafv2_rule_group.custom_rule_group_regional[0].arn
-      rules_override_to_count = []
-    }
-  ] : []
+  default_custom_managed_rule_groups_regional = (
+    length(aws_wafv2_rule_group.custom_rule_group_regional) > 0 ? [
+      {
+        name                    = "CustomManagedRuleSetRegional"
+        priority                = 1
+        action                  = "none"
+        rule_group_arn          = aws_wafv2_rule_group.custom_rule_group_regional[0].arn
+        rules_override_to_count = []
+      }
+    ] : []
+  )
 
   filtered_custom_managed_rule_groups = [
     for r in var.custom_managed_waf_rule_groups : r
@@ -30,11 +31,13 @@ locals {
     )
   ]
 
-  effective_custom_managed_waf_rule_groups = length(local.filtered_custom_managed_rule_groups) > 0
-    ? local.filtered_custom_managed_rule_groups
-    : (
-        var.web_acl_scope == "CLOUDFRONT"
-        ? local.default_custom_managed_rule_groups_cloudfront
-        : local.default_custom_managed_rule_groups_regional
-      )
+  effective_custom_managed_waf_rule_groups = (
+    length(local.filtered_custom_managed_rule_groups) > 0 ?
+    local.filtered_custom_managed_rule_groups :
+    (
+      var.web_acl_scope == "CLOUDFRONT" ?
+      local.default_custom_managed_rule_groups_cloudfront :
+      local.default_custom_managed_rule_groups_regional
+    )
+  )
 }
