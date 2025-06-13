3a798c2d7dd9d0ebd6bb0f6263f09303d5779f02
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-06-13 16:50:08 +0300

    Commented the configuration in order to test the cloudfront rule creation

--- locals.tf
+++ locals.tf
@@ -1,43 +1,43 @@
-locals {
-  default_custom_managed_rule_groups_cloudfront = (
-    var.cloudfront_true && (try(length(aws_wafv2_rule_group.custom_rule_group_global), 0) > 0) ? [
-      {
-        name                    = "CustomManagedRuleSetGlobal"
-        priority                = 1
-        action                  = "none"
-        rule_group_arn          = try(aws_wafv2_rule_group.custom_rule_group_global[0].arn, null)
-        rules_override_to_count = []
-      }
-    ] : []
-  )
+#locals {
+#  default_custom_managed_rule_groups_cloudfront = (
+#    var.cloudfront_true && (try(length(aws_wafv2_rule_group.custom_rule_group_global), 0) > 0) ? [
+#      {
+#        name                    = "CustomManagedRuleSetGlobal"
+#        priority                = 1
+#        action                  = "none"
+#        rule_group_arn          = try(aws_wafv2_rule_group.custom_rule_group_global[0].arn, null)
+#        rules_override_to_count = []
+#      }
+#    ] : []
+#  )#
 
-  default_custom_managed_rule_groups_regional = (
-    var.application_true && (try(length(aws_wafv2_rule_group.custom_rule_group_regional), 0) > 0) ? [
-      {
-        name                    = "CustomManagedRuleSetRegional"
-        priority                = 1
-        action                  = "none"
-        rule_group_arn          = try(aws_wafv2_rule_group.custom_rule_group_regional[0].arn, null)
-        rules_override_to_count = []
-      }
-    ] : []
-  )
+#  default_custom_managed_rule_groups_regional = (
+#    var.application_true && (try(length(aws_wafv2_rule_group.custom_rule_group_regional), 0) > 0) ? [
+#      {
+#        name                    = "CustomManagedRuleSetRegional"
+#        priority                = 1
+#        action                  = "none"
+#        rule_group_arn          = try(aws_wafv2_rule_group.custom_rule_group_regional[0].arn, null)
+#        rules_override_to_count = []
+#      }
+#    ] : []
+#  )#
 
-  filtered_custom_managed_rule_groups = [
-    for r in var.custom_managed_waf_rule_groups : r
-    if (
-      (var.web_acl_scope == "CLOUDFRONT" && contains(r.rule_group_arn, ":global/")) ||
-      (var.web_acl_scope == "REGIONAL" && contains(r.rule_group_arn, ":regional/"))
-    )
-  ]
+#  filtered_custom_managed_rule_groups = [
+#    for r in var.custom_managed_waf_rule_groups : r
+#    if (
+#      (var.web_acl_scope == "CLOUDFRONT" && contains(r.rule_group_arn, ":global/")) ||
+#      (var.web_acl_scope == "REGIONAL" && contains(r.rule_group_arn, ":regional/"))
+#    )
+#  ]#
 
-  effective_custom_managed_waf_rule_groups = (
-    length(local.filtered_custom_managed_rule_groups) > 0 ?
-    local.filtered_custom_managed_rule_groups :
-    (
-      var.web_acl_scope == "CLOUDFRONT" ?
-      local.default_custom_managed_rule_groups_cloudfront :
-      local.default_custom_managed_rule_groups_regional
-    )
-  )
-}
+#  effective_custom_managed_waf_rule_groups = (
+#    length(local.filtered_custom_managed_rule_groups) > 0 ?
+#    local.filtered_custom_managed_rule_groups :
+#    (
+#      var.web_acl_scope == "CLOUDFRONT" ?
+#      local.default_custom_managed_rule_groups_cloudfront :
+#      local.default_custom_managed_rule_groups_regional
+#    )
+#  )
+#}
--- output.tf
+++ output.tf
@@ -2,18 +2,18 @@ output "webacl_arn" {
   value = length(aws_wafv2_web_acl.wafv2_web_acl) > 0 ? aws_wafv2_web_acl.wafv2_web_acl[0].arn : null
 }
 
-output "webacl_id" {
-  value = length(aws_wafv2_web_acl.wafv2_web_acl) > 0 ? aws_wafv2_web_acl.wafv2_web_acl[0].id : null
-}
+#output "webacl_id" {
+#  value = length(aws_wafv2_web_acl.wafv2_web_acl) > 0 ? aws_wafv2_web_acl.wafv2_web_acl[0].id : null
+#}
 
-output "custom_rule_group_arns" {
-  value = var.cloudfront_true || var.application_true ? [
-    for rg in local.effective_custom_managed_waf_rule_groups :
-    rg.rule_group_arn
-    if rg.rule_group_arn != null
-  ] : []
-}
+#output "custom_rule_group_arns" {
+#  value = var.cloudfront_true || var.application_true ? [
+#    for rg in local.effective_custom_managed_waf_rule_groups :
+#    rg.rule_group_arn
+#    if rg.rule_group_arn != null
+#  ] : []
+#}
 
-output "debug_rule_group_count" {
-  value = var.cloudfront_true ? 1 : 0
-}
+#output "debug_rule_group_count" {
+#  value = var.cloudfront_true ? 1 : 0
+#}
