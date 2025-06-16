5ac0d6a84779d8b208b5cf9bc55726b8b6f5afd7
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-06-16 11:19:31 +0300

    Commented the configuration in order to test the cloudfront rule creation

--- webacl.tf
+++ webacl.tf
@@ -4,8 +4,7 @@ provider "aws" {
 }
 
 resource "aws_wafv2_rule_group" "custom_rule_group_global" {
-  count       = var.waf_enabled ? 1 : 0
-  count       = var.cloudfront_true ? 1 : 0
+  count       = var.waf_enabled && var.cloudfront_true ? 1 : 0
   provider    = aws.virginia
   name        = "${var.project}-${var.env}-${var.aws_region}-cloudfront-rule-group-global"
   scope       = "CLOUDFRONT"
@@ -66,8 +65,7 @@ resource "aws_wafv2_rule_group" "custom_rule_group_global" {
 }
 
 resource "aws_wafv2_rule_group" "custom_rule_group_regional" {
-  count       = var.waf_enabled ? 1 : 0
-  count       = var.application_true ? 1 : 0
+  count       = var.waf_enabled && var.application_trues ? 1 : 0
   name        = "${var.project}-${var.env}-${var.aws_region}-application-group-regional"
   scope       = "REGIONAL"
   capacity    = 50  # minimum capacity for empty rule group
