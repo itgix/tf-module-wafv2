f3d49bd5e77cce7b11363ffad81f1806358aeaca
Tsvetana Kanzova <tsvetana.kanzova@itgix.com>
2025-06-16 13:15:37 +0300

    Fixing a typo

--- locals.tf
+++ locals.tf
@@ -17,7 +17,7 @@ locals {
         name                    = "CustomManagedRuleSetRegional"
         priority                = 1
         action                  = "none"
-        rule_group_arn          = one(aws_wafv2_rule_group.custom_rule_group_global[*].arn)
+        rule_group_arn          = one(aws_wafv2_rule_group.custom_rule_group_regional[*].arn)
         rules_override_to_count = []
       }
     ] : []
